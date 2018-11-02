4fb7ccc692337f62a0aeb9c6b828f52f5f471d5e
Mat Harb <mharb@ancestry.com>
2018-11-02 10:22:49 -0600

    update note

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const uuidV4 = require('uuid/v4');
 
 const notes = [
-  { id: '1', title: 'Sample notes', data: '# Sample header' },
+  { id: '1', title: 'Sample notes', data: '# Markdown notes \n\n In order for state to persist across refresh.... Save the note' },
   { id: '2', title: 'Sample notes 2', data: '# Sample header 2' },
   { id: '3', title: 'Sample notes 3', data: '# Sample header 3' }
 ];
